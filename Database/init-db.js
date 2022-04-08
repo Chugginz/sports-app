b3194f4d0e08599437d14e68b23ddd41921a679b
Chuggins <christia.huggins@smail.smail.astate.edu>
2022-04-08 09:37:05 -0500

    NPM Installs

--- Database/init-db.js
+++ Database/init-db.js
@@ -2,7 +2,7 @@
 
 require('dotenv').config()          // Don't forget to set up environment variables
 const fs = require("fs");           // fs module grants file system access
-const db = require("../Models/db"); // We need our db connection
+const db = require("../Models/db.js"); // We need our db connection
 
 // Now read the schema.sql file into a string
 // It is ok to do this synchronously since this script executes before loading the server
