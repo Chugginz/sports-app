c3cdd0a39ce15d3a2bec632f054041ca88a2fcf7
Chuggins <christia.huggins@smail.smail.astate.edu>
2022-04-09 12:39:07 -0500

    Broken

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 const app = express();
-const dictionaryModel = require("./Models/dictionaryModel");
+// const dictionaryModel = require("./Models/dictionaryModel");
 
 
 // The maximum request body size is 100 kilobytes; however, my word list was
