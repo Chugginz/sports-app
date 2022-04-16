470b83bf33738966c387d0d6a0eea40e7681d0a7
Chuggins <christia.huggins@smail.smail.astate.edu>
2022-04-16 18:11:11 -0500

    Changed Html files

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
 require("dotenv").config();
 
 const app = require("./app");
-//const liveChat = require("./Controllers/liveChat.js");
 
 app.listen(process.env.PORT, () => {
     console.log(`Listening on port: ${process.env.PORT}`);
