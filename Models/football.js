9e18af4ec4993a720414a8a5b6cf3645f237eb27
Chuggins <christia.huggins@smail.smail.astate.edu>
2022-04-17 21:33:30 -0500

    API Info

--- Models/football.js
+++ Models/football.js
@@ -1,9 +1,27 @@
 "use strict";
 
+// Initializing requrest so that the call doesn't have to be made a lot
 let request = new XMLHttpRequest();
 
-request.open('GET', 'http:api.espn.com/v1/sports/football/nfl/events/competitions');
+// Opens the api for Dallas
+// Tested, can insert id numbers instead of team abreviations, meaning can do 1-32
+request.open('GET', `https://site.web.api.espn.com/apis/site/v2/sports/football/nfl/teams/${counter}/schedule?seasontype=2`);
 
+// Gets info
 request.onload = function getInfo (){
-    let id = request.id;
+    let teamName = request.team.displayName;
+    let teamLogoLink = request.team.logo;
+    let oppId = request.competitors.id;
+    let oppWin = request.competitors.winner;
+}
+
+for (let counter = 1; counter <= 32; counter++){
+    // Tested, can insert id numbers instead of team abreviations, meaning can do 1-32
+    request.open('GET', `https://site.web.api.espn.com/apis/site/v2/sports/football/nfl/teams/${counter}/schedule?seasontype=2`);
+    // Call function to set variables
+    request.onload();
+    if (oppId > counter){
+        // If the opponent hasn't been shown yet, display the teams
+
+    }
 }
