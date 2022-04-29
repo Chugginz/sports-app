b23538e6c195703f231306d31379c69910b7b9af
Jacob Bell <jacob.bell@smail.astate.edu>
2022-04-29 18:21:42 -0500

    added login and session management

---
+++ Validators/makeValidator.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function makeValidator (schema) {
+    return function (req, res, next) {
+        const {value, error} = schema.validate(req.body, {
+            abortEarly: false,
+            stripUnknown: true, 
+            errors: {
+                escapeHtml: true,
+            }
+        });
+        
+        if (error) {
+            const errorMessages = error.details.map(detail => detail.message);
+            return res.status(400).json({errorMessages});
+        } 
+
+        req[prop] = value;
+        
+        next();
+    }
+}
+
+module.exports = {
+    makeValidator,
+};
--- app.js
+++ app.js
@@ -4,7 +4,31 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const redis = require('redis');
+const session = require('express-session');
+
+let RedisStore = require('connect-redis')(session);
+let redisClient = redis.createClient({
+    auth_pass: process.env.AUTH_PASSWORD,
+});
+
+const sessionConfig = {
+  store: new RedisStore({ client: redis.createClient() }),
+  secret: process.env.COOKIE_SECRET, 
+  resave: false,
+  saveUninitialized: false,
+  name: "session", // now it is just a generic name
+  cookie: {
+    httpOnly: true,
+    maxAge: 1000 * 60 * 60 * 8, // 8 hours
+  },
+};
+
+app.use(session(sessionConfig))
+
 app.use(express.static(__dirname + '/public'));
+
+app.use(express.json({limit: '200kb'}))
 
 /*app.post("/api/footballScores", (req,res) => {
     loadInfo();
@@ -14,7 +38,18 @@ app.set('view engine', 'ejs');
 
 // Load controllers
 const footballController = require('./Controllers/footballController');
+const userController     = require("./Controllers/userController")
+
+// Load Validators
+const userValidator = require("./Validators/userValidator");
+
+
+const { application } = require("express");
+
 
 app.get("/football/scores", footballController.renderScores);
+app.post("/api/user", userValidator.userValidator, userController.createNewUser)
+app.post("/api/login", userValidator.userValidator, userController.login)
+
 
 module.exports = app;
