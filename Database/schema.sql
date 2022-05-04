23ad793d54f7b201a27423f3579f86c16534c5e5
loltexas <98758618+loltexas@users.noreply.github.com>
2022-05-03 23:28:02 -0500

    fixed spell errors

--- Database/schema.sql
+++ Database/schema.sql
@@ -17,6 +17,19 @@ CREATE TABLE IF NOT EXISTS Football (
     CONSTRAINT teamInfo UNIQUE(teamname, week)
 );
 
+CREATE TABLE IF NOT EXISTS Basketball (
+    teamid          TEXT NOT NULL,
+    teamname        TEXT NOT NULL,
+    logo            TEXT NOT NULL,
+    record          TEXT NOT NULL,
+    week            TEXT NOT NULL,
+    score           TEXT NOT NULL,
+    winStatus       TEXT NOT NULL,
+    visitStatus     TEXT NOT NULL,
+    opponentid      INTEGER NOT NULL,
+    CONSTRAINT teamInfo UNIQUE(teamname, week)
+);
+
 CREATE TABLE IF NOT EXISTS News (
     contentid       TEXT PRIMARY KEY,
     img             TEXT NOT NULL,
